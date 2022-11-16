8e75c445b005907b7f0e3ab0abc2e20add8088d2
석도형 <zxc1460@mailplug.co.kr>
2022-11-16 18:30:02 +0900

    기본 ViewController 제거, TagCell 추가

---
+++ Jobplanet-test/Jobplanet-test/Views/TagCell.swift
@@ -0,0 +1,22 @@
+//
+//  TagCell.swift
+//  Jobplanet-test
+//
+//  Created by DoHyeong on 2022/11/16.
+//
+
+import UIKit
+
+class TagCell: UICollectionViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    func configure(title: String) {
+        self.titleLabel.text = title
+    }
+}
